6e7dec45151596378abaf52dcaf20333a8f35285
jinjernot <norindes@gmail.com>
2025-11-06 14:12:48 -0600

    granular group check fixed

--- app/routes/scs_tool/core/qa_data.py
+++ app/routes/scs_tool/core/qa_data.py
@@ -24,13 +24,15 @@ def clean_report(file):
         df = pd.read_excel(file_buffer, engine='openpyxl')
         df = df.drop(SCS_COLS_TO_ADD, axis=1, errors='ignore')
         df[SCS_COLS_TO_ADD] = ''
-        
+
         pl_check(df)
 
         # Basic data cleaning
-        df = df[df['ContainerValue'] != '[BLANK]'].dropna(subset=['ContainerValue', 'ContainerName'])
+        df = df[df['ContainerValue'] != '[BLANK]'].dropna(
+            subset=['ContainerValue', 'ContainerName'])
         df.replace('\u00A0', ' ', regex=True, inplace=True)
-        df.loc[df['ContainerValue'].str.endswith(';', na=False), 'ContainerValue'] = df['ContainerValue'].str.slice(stop=-1)
+        df.loc[df['ContainerValue'].str.endswith(
+            ';', na=False), 'ContainerValue'] = df['ContainerValue'].str.slice(stop=-1)
         df['PhwebDescription'] = df['PhwebDescription'].str.lstrip()
         df['ContainerValue'] = df['ContainerValue'].astype(str)
 
@@ -38,13 +40,15 @@ def clean_report(file):
         with open(SCS_COMPONENT_GROUPS_PATH, 'r', encoding='utf-8') as json_file:
             json_data = json.load(json_file)
         groups = json_data['Groups']
-        
-        filtered_rows = df[df.apply(lambda row: any(row['ComponentGroup'] == group['ComponentGroup'] and row['ContainerName'] in group['ContainerName'] for group in groups), axis=1)]
+
+        filtered_rows = df[df.apply(lambda row: any(row['ComponentGroup'] == group['ComponentGroup']
+                                    and row['ContainerName'] in group['ContainerName'] for group in groups), axis=1)]
         df = filtered_rows.copy()
 
         # --- 3. Main Data Processing Loop ---
-        json_files = [f for f in os.listdir(SCS_JSON_PATH) if f.endswith('.json')]
-        
+        json_files = [f for f in os.listdir(
+            SCS_JSON_PATH) if f.endswith('.json')]
+
         for json_file in json_files:
             container_name = os.path.splitext(json_file)[0]
             json_file_path = os.path.join(SCS_JSON_PATH, json_file)
@@ -52,7 +56,7 @@ def clean_report(file):
 
         # --- 4. NPU Validation Step ---
         df = npu_check(df, NPU_JSON_PATH)
-        
+
         # --- 5. Save Output ---
         file_buffer.seek(0)
         with pd.ExcelFile(file_buffer, engine='openpyxl') as excel_file:
@@ -61,7 +65,8 @@ def clean_report(file):
                 df_final = av_check(file_buffer)
                 with pd.ExcelWriter(SCS_REGULAR_FILE_PATH) as writer:
                     df.to_excel(writer, sheet_name='qa', index=False)
-                    df_final.to_excel(writer, sheet_name='duplicated', index=False)
+                    df_final.to_excel(
+                        writer, sheet_name='duplicated', index=False)
             else:
                 df.to_excel(SCS_REGULAR_FILE_PATH, index=False)
 
@@ -86,23 +91,34 @@ async def clean_report_granular(file):
         df_g = df_g.drop(SCS_COLS_TO_DROP_GRANULAR, axis=1, errors='ignore')
         df_g[SCS_COLS_TO_ADD] = ''
 
-        df_g = df_g.dropna(subset=['Granular Container Value', 'Granular Container Tag'])
+        df_g = df_g.dropna(
+            subset=['Granular Container Value', 'Granular Container Tag'])
         df_g.replace('\u00A0', ' ', regex=True, inplace=True)
-        df_g.loc[df_g['Granular Container Value'].str.endswith(';', na=False), 'Granular Container Value'] = df_g['Granular Container Value'].str.slice(stop=-1)
-        df_g['Granular Container Value'] = df_g['Granular Container Value'].astype(str)
+        df_g.loc[df_g['Granular Container Value'].str.endswith(
+            ';', na=False), 'Granular Container Value'] = df_g['Granular Container Value'].str.slice(stop=-1)
+        df_g['Granular Container Value'] = df_g['Granular Container Value'].astype(
+            str)
 
         # --- 2. Data Processing Loop ---
-        json_files = [f for f in os.listdir(SCS_JSON_GRANULAR_PATH) if f.endswith('.json')]
-        
+        json_files = [f for f in os.listdir(
+            SCS_JSON_GRANULAR_PATH) if f.endswith('.json')]
+
+        with open(SCS_GRANULAR_COMPONENT_GROUPS_PATH, 'r', encoding='utf-8') as json_file:
+            json_data = json.load(json_file)
+        groups = json_data['Groups']
+
+        filtered_rows = df_g[df_g.apply(lambda row: any(row['SCSGroup'] == group['ComponentGroup']
+                                        and row['Granular Container Tag'] in group['ContainerName'] for group in groups), axis=1)]
+        df_g = filtered_rows.copy()
+
         for json_file in json_files:
             container_name = os.path.splitext(json_file)[0]
             json_file_path = os.path.join(SCS_JSON_GRANULAR_PATH, json_file)
-            
+
             df_g = process_data_granular(json_file_path, container_name, df_g)
 
         # --- 3. Final Checks and Save ---
         df_g = check_missing_fields(df_g, SCS_GRANULAR_COMPONENT_GROUPS_PATH)
-       
 
         file_buffer.seek(0)
         with pd.ExcelFile(file_buffer, engine='openpyxl') as excel_file:
@@ -112,7 +128,8 @@ async def clean_report_granular(file):
                 df_final = av_check(file_buffer)
                 with pd.ExcelWriter(SCS_GRANULAR_FILE_PATH) as writer:
                     df_g.to_excel(writer, sheet_name='qa', index=False)
-                    df_final.to_excel(writer, sheet_name='duplicated', index=False)
+                    df_final.to_excel(
+                        writer, sheet_name='duplicated', index=False)
             else:
                 df_g.to_excel(SCS_GRANULAR_FILE_PATH, index=False)
 
