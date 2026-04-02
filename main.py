06f5677eb9180e616f2d4bc093a6efc186c823d2
jinjernot <norindes@gmail.com>
2026-04-02 11:15:39 -0600

    app removal

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
 
-from app.routes.image_tool.route_image import image_tool
 from app.routes.scs_tool.route_scs import scs_tool
 from app.routes.qs_tool.route_qs import qs_tool
-from app.routes.ds_tool.route_ds import ds_tool
 
 import config
 
@@ -25,16 +23,6 @@ def scs_tool_route():
 def qs_tool_route():
     """QS Tool page"""
     return qs_tool()
-
-@app.route('/image_tool', methods=['GET', 'POST'])
-def image_tool_route():
-    """Image Tool page"""
-    return image_tool()
-    
-@app.route('/ds_tool', methods=['GET', 'POST'])
-def ds_tool_route():
-    """DS Tool page"""
-    return ds_tool()
 
 @app.route('/faq', methods=['GET', 'POST'])
 def faq_route():
